f52ea7cba563b23db1062b09c45586fbcc0465b8
TU CAM KIET <camkiettu@gmail.com>
2026-06-23 14:55:28 +0700

    Api Host

--- frontend/src/api/landingApi.js
+++ frontend/src/api/landingApi.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000",
 });
 
 export const getLandingData = async () => {
   const response = await api.get("/landing");
   console.log("front", response.data);
-
   return response.data;
 };
